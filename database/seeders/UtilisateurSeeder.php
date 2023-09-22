f065a7860432122e39daf1bdc02b3a8732bfdd17
dakhamme474 <dakhamme474@gmail.com>
2023-09-22 10:53:26 +0100

    useres

---
+++ database/seeders/UtilisateurSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UtilisateurSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create( [
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123456789', // password
+        ])->assignRole('admin');
+
+        User::create( [
+            'name' => 'dakhamme',
+            'email' => 'dakhamme@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123456789', // password
+        ])->assignRole('user');
+
+        User::create( [
+            'name' => 'webmaster',
+            'email' => 'webmaster@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '123456789', // password
+        ])->assignRole('webmaster');
+    }
+}
